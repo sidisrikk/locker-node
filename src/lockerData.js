09ac66fa7976261ffc8406a98323bf4ea1bca38f
sidi <s>
2019-03-11 16:23:08 +0700

    global lockerdata

---
+++ src/lockerData.js
@@ -0,0 +1,146 @@
+global.globalLockerInfo = {
+  'lockerInfo': {
+    'no': 1,
+    'unit': [
+      {
+        'no': 1,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 2,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 3,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 4,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 5,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 6,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 7,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 8,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 9,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 10,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 11,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 12,
+        'passcode': '',
+        'status': 'available',
+      },
+    ],
+  },
+};
+const setLockerUnitInfo = (data) => {
+  global.globalLockerInfo = data;
+};
+
+const getLockerUnitInfo = () => global.globalLockerInfo;
+
+const mock = {
+  'lockerInfo': {
+    'no': 1,
+    'unit': [
+      {
+        'no': 1,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 2,
+        'passcode': '123321',
+        'status': 'reserved',
+      },
+      {
+        'no': 3,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 4,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 5,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 6,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 7,
+        'passcode': '064463',
+        'status': 'reserved',
+      },
+      {
+        'no': 8,
+        'passcode': '571804',
+        'status': 'reserved',
+      },
+      {
+        'no': 9,
+        'passcode': '820533',
+        'status': 'reserved',
+      },
+      {
+        'no': 10,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 11,
+        'passcode': '',
+        'status': 'available',
+      },
+      {
+        'no': 12,
+        'passcode': '',
+        'status': 'available',
+      },
+    ],
+  },
+};
+
+export {
+  mock,
+  setLockerUnitInfo,
+  getLockerUnitInfo,
+};
